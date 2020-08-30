86de88a35636a2ec4bd57469771f39e4712ef394
Allen Mackey <allenmackey@armackey.attlocal.net>
2020-08-29 21:34:43 -0700

    fixed dist issue

--- dist/methods.js
+++ dist/methods.js
@@ -12,6 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.find = exports.remove = exports.findByName = exports.findByEmail = exports.create = exports.update = exports.findAll = exports.findOneById = exports.CommonMongooseMethods = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const SCHEMAS = {};
 class CommonMongooseMethods {
